06b35841f87cb2e0d2f1bbaf397683bec278643b
kirubel-Nigussie <kiranigussie1738@gmail.com>
2024-08-12 13:10:38 +0300

    first commit - committing all files to the repository

---
+++ main.js
@@ -0,0 +1,104 @@
+var menuList= document.getElementById("menuList");
+menuList.style.maxHeight="0px";
+
+function togglemenu(){
+
+    if(menuList.style.maxHeight=="0px"){
+        menuList.style.maxHeight="350px";
+    }
+    else{
+        menuList.style.maxHeight="0px";
+    }
+}
+
+
+
+
+        const form = document.getElementById('form');
+        const username = document.getElementById('username');
+        const email = document.getElementById('email');
+        const password = document.getElementById('password');
+        const password2 = document.getElementById('password2');
+        
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+        
+            validateInputs();
+        });
+        
+        const setError = (element, message) => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+        
+            errorDisplay.innerText = message;
+            inputControl.classList.add('error');
+            inputControl.classList.remove('success')
+        }
+        
+        const setSuccess = element => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+        
+            errorDisplay.innerText = '';
+            inputControl.classList.add('success');
+            inputControl.classList.remove('error');
+        };
+        
+        const isValidEmail = email => {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        }
+        
+        const validateInputs = () => {
+            const usernameValue = username.value.trim();
+            const lastNameValue = lastName.value.trim();
+            const emailValue = email.value.trim();
+            const phonNumberValue = phonNumber.value.trim();
+            const passwordValue = password.value.trim();
+            const password2Value = password2.value.trim();
+        
+            if(usernameValue === '') {
+                setError(username, 'Username is required');
+            } else {
+                setSuccess(username);
+            }
+            if(lastNameValue === '') {
+                setError(lastName, 'lastName is required');
+            } else {
+                setSuccess(lastName);
+            }
+        
+            if(emailValue === '') {
+                setError(email, 'Email is required');
+            } else if (!isValidEmail(emailValue)) {
+                setError(email, 'Provide a valid email address');
+            } else {
+                setSuccess(email);
+            }
+        
+            if(passwordValue === '') {
+                setError(password, 'Password is required');
+            } else if (passwordValue.length < 8 ) {
+                setError(password, 'Password must be at least 8 character.')
+            } else {
+                setSuccess(password);
+            }
+        
+            if(password2Value === '') {
+                setError(password2, 'Please confirm your password');
+            } else if (password2Value !== passwordValue) {
+                setError(password2, "Passwords doesn't match");
+            } else {
+                setSuccess(password2);
+            }
+            if(phonNumberValue === '') {
+                setError(phonNumber, 'phonNumber is required');
+            } else {
+                setSuccess(phonNumber);
+            }
+        
+        };
+        
+             
+
+        
